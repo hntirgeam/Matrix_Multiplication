3d36e3a298d6de94884cc5999c7dd8b4af738d57
hntirgeam <kmacroviperaz@gmail.com>
2020-06-25 01:30:08 +0300

    yeah, multithreading

--- src/WorkerClass/Worker.cpp
+++ src/WorkerClass/Worker.cpp
@@ -1,63 +1,27 @@
 #include "Worker.hpp"
 
-void Worker::multiply(const int &slice)
+void Worker::multiply(const size_t &slice)
 {
 
-    const int from = (slice * m_dim) / num_thread;
-    const int to = ((slice + 1) * m_dim) / num_thread;
+    const size_t from = (slice * m_dim) / num_thread;
+    const size_t to = ((slice + 1) * m_dim) / num_thread;
 
-    for (int i = from; i < to; i++)
+    for (size_t i = from; i < to; i++)
     {
-        for (int j = 0; j < m_dim; j++)
+        for (size_t j = 0; j < m_dim; j++)
         {
-            tmp(i, j) = 0; // initialize matrix C
-            for (int k = 0; k < m_dim; k++)
+            tmp(i, j) = 0;
+            for (size_t k = 0; k < m_dim; k++)
                 tmp(i, j) += m1(i, k) * m2(k, j);
         }
     }
 }
 
-// void Worker::multiply1()
-// {
-//     std::cout << "m_dim = " << m_dim << std::endl;
-//     std::cout << "m1 size = " << m1.getVecSize() << std::endl;
-//     std::cout << "m2 size = " << m2.getVecSize() << std::endl;
-//     std::cout << "tmp size = " << tmp.getVecSize() << std::endl;
-
-//     for (size_t i = 0; i < m_dim; i++)
-//     {
-//         for (size_t j = 0; j < m_dim; j++)
-//         {
-//             tmp(i, j) = 0;
-//             for (size_t k = 0; k < m_dim; k++)
-//             {
-//                 tmp(i, j) += m1(i, k) * m2(k, j);
-//             }
-//         }
-//     }
-// }
-
 Matrix Worker::calculate()
 {
-    // boost::thread threads[t_num];
-    // std::function<void()> func;
-    // auto func = [this]() {
-    //     int core = step_i++;
-    //     for (int i = core * m_dim / t_num; i < (core + 1) * m_dim / t_num; i++)
-    //     {
-    //         for (int j = 0; j < m_dim; j++)
-    //         {
-    //             for (int k = 0; k < m_dim; k++)
-    //             {
-    //                 tmp(i, j) += m1(i, k) * m2(k, j);
-    //             }
-    //         }
-    //     }
-    // };
-
     boost::thread_group threads;
-
-    for (int i = 0; i < num_thread; i++)
+    std::cout << "Calculating on " << num_thread << " thread(s)" << std::endl;
+    for (size_t i = 0; i < num_thread; i++)
     {
         threads.create_thread(boost::bind(&Worker::multiply, this, i));
     }
--- src/WorkerClass/Worker.hpp
+++ src/WorkerClass/Worker.hpp
@@ -8,16 +8,15 @@
 class Worker
 {
 public:
-    Worker(size_t threadAmount, const Matrix &_m1, const Matrix &_m2, const Matrix &_tmp) : 
-        num_thread(threadAmount), m1(_m1), m2(_m2), tmp(_tmp), m_dim(m1.getRow())
+    Worker(size_t threadAmount, const Matrix &_m1, const Matrix &_m2, const Matrix &_tmp)
+        : num_thread(threadAmount), m1(_m1), m2(_m2), tmp(_tmp), m_dim(m1.getRow())
     {
     }
     ~Worker()
     {
     }
 
-    void multiply(const int &slice);
-    void multiply1();
+    void multiply(const size_t &slice);
 
     Matrix calculate();
 
@@ -27,5 +26,4 @@ private:
 
     size_t m_dim;      //  matrix dimension
     size_t num_thread; // thread amount
-    size_t step_i = 0;
 };
